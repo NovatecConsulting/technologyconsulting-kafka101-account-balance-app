7f445fe3eb78a835d158475e007dca37029f960b
Hieu Vo <duyhieu.vo@notatec-gmbh.de>
2019-11-13 10:34:51 +0100

    Workaround the bug on Windows of closing Kafka Streams TopologyTestDriver: manually delete test state store folder.

--- src/test/java/kafka101/kafka/TestProcessorTopology.java
+++ src/test/java/kafka101/kafka/TestProcessorTopology.java
@@ -15,11 +15,14 @@ import org.apache.kafka.streams.TopologyTestDriver;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.apache.kafka.streams.test.ConsumerRecordFactory;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 public class TestProcessorTopology {
@@ -33,33 +36,39 @@ public class TestProcessorTopology {
         Topology topology = processor.kafkaStreamTopology();
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test-stream-processor");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
+        props.setProperty(StreamsConfig.STATE_DIR_CONFIG,"test-state-store");
         testDriver = new TopologyTestDriver(topology, props);
         store = testDriver.getKeyValueStore(KafkaConfiguration.STREAM_STATE_STORE);
     }
 
     @After
-    public void tearDown(){
-        //testDriver.close(); //Unresolved bug on windows
-        testDriver = null;
-        store = null;
+    public void tearDown() throws IOException {
+        try{
+            testDriver.close(); //unresolved bug on windows
+        }catch(Exception e){
+            FileUtils.deleteDirectory(new File("test-state-store")); //workaround: manually delete the state store folder of the test
+        }
     }
 
     @Test
     public void testTopology(){
         ConsumerRecordFactory<Integer, Event> factory = new ConsumerRecordFactory<>(KafkaConfiguration.TOPIC_NAME,  new IntegerSerializer(), new EventSerializer());
 
-        testDriver.pipeInput(factory.create(1, new WithdrawEvent(1, 100)));
+        testDriver.pipeInput(factory.create(1, new DepositEvent(1, 100)));
+        testDriver.pipeInput(factory.create(2, new WithdrawEvent(2, 100)));
+        testDriver.pipeInput(factory.create(1, new WithdrawEvent(1, 200)));
+        testDriver.pipeInput(factory.create(2, new DepositEvent(2, 100)));
+
         int amount = store.get(1);
         Assert.assertEquals(-100,amount);
 
-
-        testDriver.pipeInput(factory.create(1, new DepositEvent(1, 100)));
-        amount = store.get(1);
+        amount = store.get(2);
         Assert.assertEquals(0,amount);
+
     }
 
 
